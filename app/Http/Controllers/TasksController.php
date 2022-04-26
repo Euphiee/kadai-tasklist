ffe1b018e1787e4bdfd23d555b7a67493751fa12
tracy.li <1991tracyli@gmail.com>
2022-04-26 10:55:17 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -38,6 +38,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'content'=>'required|max:10',
+            'status'=>'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->status = $request->status;
@@ -78,6 +83,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'content'=>'required|max:10',
+            'status'=>'required|max:10',
+        ]);
+        
         $task = Task::findOrFail($id);
         $task->content = $request->content;
         $task->status = $request->status;
