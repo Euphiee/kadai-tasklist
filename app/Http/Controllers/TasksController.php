b149879b22fbf33605db4baa16b221f7f8ad83e5
tracy.li <1991tracyli@gmail.com>
2022-04-26 10:46:04 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -40,6 +40,7 @@ class TasksController extends Controller
     {
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         return redirect('/');
     }
@@ -79,6 +80,7 @@ class TasksController extends Controller
     {
         $task = Task::findOrFail($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         return redirect('/');
     }
